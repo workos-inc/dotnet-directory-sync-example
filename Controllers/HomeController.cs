35c6733c274ffb0cc0a5464e159de17a5364ab6f
Logan Gingerich <logangingerich@yahoo.com>
2021-09-20 16:18:15 -0600

    Add inline comments

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -24,8 +24,8 @@ namespace WorkOS.DSyncExampleApp.Controllers
 
         public IActionResult Index()
         {
+            //Pull and store Directory ID from environment variables.
             var directoryId = Environment.GetEnvironmentVariable("WORKOS_DIRECTORY_ID");
-
             ViewBag.Id = directoryId;
 
             return View();
@@ -36,17 +36,17 @@ namespace WorkOS.DSyncExampleApp.Controllers
         {
             // Initialize the WorkOS client with your WorkOS API Key.
             WorkOS.SetApiKey(Environment.GetEnvironmentVariable("WORKOS_API_KEY"));
+            // Initialize WorkOS Directory Service.
             var directorySync = new DirectorySyncService();
-
+            // Set Directory ID as option for our API call
             var options = new ListUsersOptions
             {
                 Directory = Environment.GetEnvironmentVariable("WORKOS_DIRECTORY_ID"),
             };
-
+            // API Call to list all users within our specified directory.
             WorkOSList<User> UserList = await directorySync.ListUsers(options);
-
+            // Parse response and send to view.
             var users = JObject.Parse(JsonSerializer.Serialize(UserList))["Data"];
-
             ViewBag.Users = users;
 
 
@@ -58,17 +58,17 @@ namespace WorkOS.DSyncExampleApp.Controllers
         {
             // Initialize the WorkOS client with your WorkOS API Key.
             WorkOS.SetApiKey(Environment.GetEnvironmentVariable("WORKOS_API_KEY"));
+            // Initialize WorkOS Directory Service.
             var directorySync = new DirectorySyncService();
-
+            // Set Directory ID as option for our API call.
             var groupsOptions = new ListGroupsOptions
             {
                 Directory = Environment.GetEnvironmentVariable("WORKOS_DIRECTORY_ID"),
             };
-
+            // API Call to list all groups within our specified directory.
             WorkOSList<Group> GroupList = await directorySync.ListGroups(groupsOptions);
-
+            // Parse response and send to view.
             var groups = JObject.Parse(JsonSerializer.Serialize(GroupList))["Data"];
-
             ViewBag.Groups = groups;
 
             return View();
